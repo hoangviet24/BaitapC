9b22b4ec7e8b61ddf4d91aff7be03984a3d502b7
vo viet <hoangvietdoican2004@gmail.com>
2022-10-16 16:00:53 +0700

    BaitapC#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Bài tập căn bậc 2
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Input y: ");
+            float y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("z = "+(2*x + Math.Sqrt(y))/13);
+            Console.WriteLine(" ");
+            Console.WriteLine(Math.Sqrt(x));
+        }
+    }
+}
