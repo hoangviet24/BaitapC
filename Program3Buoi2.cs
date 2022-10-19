3eee23951a3acabeb8f02c45b0f4b4dbd4944641
vo viet <90944284+hoangviet24@users.noreply.github.com>
2022-10-19 22:31:27 +0700

    Update Program3Buoi2.cs

--- Program3Buoi2.cs
+++ Program3Buoi2.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
+using System;
 namespace Baitap3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float n;
+            int n;
             double c, dv, t;
-            Console.WriteLine("Input n < 1000");
-            n = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Input integer n < 1000");
+            n = Convert.ToInt32(Console.ReadLine());
             c = (n % 100) / 10;
             dv = (n % 100) % 10;
             t = n / 100;
@@ -20,9 +16,9 @@ namespace Baitap3
             Console.WriteLine("t = {0}", t);
             Console.WriteLine("c = {0}", c);
             Console.WriteLine("dv = {0}", dv);
-            if (c == 0 && dv != 0)
+            if (c %2 == 0 && dv %2 != 0)
                 Console.WriteLine("odd number");
-            if (c == 0 && dv == 0)
+            if (c % 2 == 0 && dv % 2 == 0)
                 Console.WriteLine("even number");
             else
                 Console.WriteLine("No answer");
