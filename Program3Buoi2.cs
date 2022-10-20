51afda2432e2a4fdcc46da4b74fba229929c0f0c
vo viet <90944284+hoangviet24@users.noreply.github.com>
2022-10-20 08:52:18 +0700

    Update Program3Buoi2.cs

--- Program3Buoi2.cs
+++ Program3Buoi2.cs
@@ -1,34 +1,69 @@
 using System;
-namespace Baitap3
+namespace BT3
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
             int n;
-            double c, dv, t;
-            Console.WriteLine("Input integer n < 1000");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            if (n > 1000)
-                Console.WriteLine("Read command. Please.");
-            if (n < 1000)
+            do {
+                Console.WriteLine("Nhap vao so nguyen co 3 chu so");
                 n = Convert.ToInt32(Console.ReadLine());
-                c = (n % 100) / 10;
-                dv = (n % 100) % 10;
-                t = n / 100;
-                Console.WriteLine("t = {0}", t);
-                Console.WriteLine("c = {0}", c);
-                Console.WriteLine("dv = {0}", dv);
-                if (c == 0 && dv != 0)
-                Console.WriteLine("odd number");
-                    else if (c == 0 && dv == 0)
-                    Console.WriteLine("even number");
-                    else
-                    Console.WriteLine("No answer");
-            Console.ReadKey();
+            }
+            while (n < 100 || n > 999);
 
+            int t = n / 100;
+            int c = (n % 100) / 10;
+            int dv = (n % 100) % 10;
+            if (c == 0 && dv == 0)
+            {
+                Console.WriteLine("Mot tram chan");
+            }
+            else { 
+            switch (t)
+            {
+                case 1: Console.Write("Mot tram "); break;
+                case 2: Console.Write("Hai tram "); break;
+                case 3: Console.Write("Ba tram "); break;
+                case 4: Console.Write("Bon tram "); break;
+                case 5: Console.Write("Nam tram "); break;
+                case 6: Console.Write("Sau tram "); break;
+                case 7: Console.Write("Bay tram "); break;
+                case 8: Console.Write("Tam tram "); break;
+                case 9: Console.Write("Chin tram "); break;
+            }
+            }
 
+            if ( c==0 && dv!=0){
+                Console.Write("le ");
+            }
+            else { 
+            switch (c)
+            {
+                case 1: Console.Write("muoi "); break;
+                case 2: Console.Write("hai muoi "); break;
+                case 3: Console.Write("ba muoi "); break;
+                case 4: Console.Write("bon muoi  "); break;
+                case 5: Console.Write("nam muoi "); break;
+                case 6: Console.Write("sau muoi "); break;
+                case 7: Console.Write("bay muoi "); break;
+                case 8: Console.Write("tam muoi "); break;
+                case 9: Console.Write("chin muoi "); break;
+            }
+            }
+            switch (dv)
+            {
+                case 1: Console.Write("mot "); break;
+                case 2: Console.Write("hai "); break;
+                case 3: Console.Write("ba "); break;
+                case 4: Console.Write("bon "); break;
+                case 5: Console.Write("nam "); break;
+                case 6: Console.Write("sau "); break;
+                case 7: Console.Write("bay "); break;
+                case 8: Console.Write("tam "); break;
+                case 9: Console.Write("chin "); break;
+            }
         }
     }
 }
+
