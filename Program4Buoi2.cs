10fb08bc73db41b6dd96aa2b240f0df8a4b4e647
vo viet <90944284+hoangviet24@users.noreply.github.com>
2022-10-20 09:05:59 +0700

    Update Program4Buoi2.cs

--- Program4Buoi2.cs
+++ Program4Buoi2.cs
@@ -6,36 +6,26 @@ namespace Baitap4
     {
         static void Main(string[] dungcodeaunua)
         {
-            Console.WriteLine("Input day in week: ");
             int day_in_week;
-            day_in_week = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Nhap thu trong tuan: ");
+                day_in_week = Convert.ToInt32(Console.ReadLine());
+            }
+            while ((day_in_week <= 1 || day_in_week > 8));
+
+           
             switch (day_in_week)
             {
-                case 2:
-                    Console.WriteLine("Monday");
-                    break;
-                case 3:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 5:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 6:
-                    Console.WriteLine("Friday");
-                    break;
-                case 7:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 8:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Looking forward to the Weekend.");
-                    break;
-
+                case 2:Console.WriteLine("Monday");break;
+                case 3:Console.WriteLine("Tuesday");break;
+                case 4:Console.WriteLine("Wednesday");break;
+                case 5:Console.WriteLine("Thursday");break;
+                case 6:Console.WriteLine("Friday");break;
+                case 7:Console.WriteLine("Saturday");break;
+                case 8:Console.WriteLine("Sunday");break;
+                case 9:Console.WriteLine("No command");break;
+                
             }
         }
     }
